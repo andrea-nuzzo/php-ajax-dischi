950e8be805ed898aba53193dd49ede29a55f7645
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-01-20 14:46:15 +0100

    completed backend

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,10 @@
 <?php
+    // Includo il database di dischi
     include __DIR__ . "/dischi.php";
 
-   header('Content-Type: application/json');
-
-   echo json_encode($database);
+    // Dichiaro che le informazioni restituite nella risposta HTTP sono di tipo json
+    header('Content-Type: application/json');
+    
+    // Attraverso la funzione json_encode traduco il database in formato json
+    echo json_encode($database);
 ?>
